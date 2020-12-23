5f1a96d62a6ac81c97fecf5bfdc90ff91b8035ec
erikbackman <erikbackman@users.noreply.github.com>
2020-12-23 15:56:49 +0100

    Day 6 part 2

--- day6/Main.hs
+++ day6/Main.hs
@@ -2,16 +2,25 @@ module Main where
 
 import Control.Arrow (Arrow((&&&)))
 import Data.List.Split (splitOn)
-import Data.List (nub)
+import Control.Monad (join)
+import Data.List (nub, intersect)
 
-parseInput :: String -> [String]
-parseInput = fmap (filter (`notElem` "\n")) . splitOn "\n\n"
+type Answers = String
+type Group = [Answers]
 
-solveP1 :: [String] -> Int
-solveP1 = sum . fmap (length . nub)
+parseInput :: String -> [Group]
+parseInput = fmap (filter (not . null) . splitOn "\n") . splitOn "\n\n"
 
-solveP2 :: [String] -> ()
-solveP2 = const ()
+solveP1 :: [Group] -> Int
+solveP1 = sum . fmap (length . nub . join)
+
+solveP2 :: [[String]] -> Int
+solveP2 = sum . fmap answers
+  where
+    answers [x] = length x
+    answers xs  = (length . same) xs
+
+    same (x:xs) = foldr intersect x xs
 
 main :: IO ()
 main = print . (solveP1 &&& solveP2) . parseInput =<< readFile "./day6inp.txt"
